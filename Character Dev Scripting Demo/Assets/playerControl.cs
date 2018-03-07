5fdf4fcf0231e9f2cb5eee56b0ea0e96c086b1e9
Mitch <mitch.d.diamond@gmail.com>
2018-03-06 22:55:21 -0700

    Pushing State Machine with FMOD Scriptable Objects. 3/6/18

---
+++ Character Dev Scripting Demo/Assets/playerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isJump", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isWalk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isJump", true);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isWalk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isJump", false);
+            anim.SetBool("isRun", true);
+            anim.SetBool("isWalk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isJump", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isWalk", true);
+        }
+    }
+}
